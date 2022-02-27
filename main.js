8fbe02fa50bfca7d90f4308767381b0937899f69
gitmibrahim <muhammed.ibrahim.me@gmail.com>
2022-02-27 09:29:23 +0200

    made the app installable

--- main.js
+++ main.js
@@ -59,3 +59,10 @@ const toggleButtonAndVideoDisplay = () => {
   }
 }
 
+// code to register sw.js
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/qr_code_scanner/sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('qrcode-scanner').then((cache) => cache.addAll([
+      '/qr_code_scanner/',
+      '/qr_code_scanner/index.html',
+      '/qr_code_scanner/main.js',
+      '/qr_code_scanner/style.css',
+      '/qr_code_scanner/icons/qr-code-192.png',
+      '/qr_code_scanner/icons/qr-code-favicon.ico',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
