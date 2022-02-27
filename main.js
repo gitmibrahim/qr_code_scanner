1a3583538ec8b53b6c9e8a76907bea1578e33737
gitmibrahim <muhammed.ibrahim.me@gmail.com>
2022-02-27 11:29:12 +0200

    reading from a file

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 
 
+const buttonsContainerElem = document.querySelector('.buttons')
 const startButtonElem = document.querySelector('button')
 const videoElem = document.querySelector('video')
+const imgElem = document.querySelector('img')
 const resultsElem = document.querySelector('#results')
-
+const file = document.querySelector('#file');
 
 let latestResult = ''
 let latestPrintedResult
@@ -11,12 +13,25 @@ let duplicateCount = 1
 let duplicateSpan
 
 startButtonElem.addEventListener('click', () => {
-  toggleButtonAndVideoDisplay()
+  toggleButtonAndVideoDisplay('cam')
   qrScanner.start()
 })
 
 
 const qrScanner = new QrScanner(videoElem, ({data}) => {
+  handleTheScan(data, 'cam')
+}, {
+  returnDetailedScanResult: true,
+  highlightScanRegion: true
+});
+
+file.addEventListener('change', () => {
+  QrScanner.scanImage(file.files[0], {returnDetailedScanResult: true})
+    .then(({data}) => handleTheScan(data, 'file'))
+    .catch(error => console.log(error || 'No QR code found.'));
+})
+
+function handleTheScan(data, media) {
   if (data !== latestResult) {
     resultsElem.style.display = 'inline-block'
 
@@ -41,27 +56,38 @@ const qrScanner = new QrScanner(videoElem, ({data}) => {
     }
   }
 
-  toggleButtonAndVideoDisplay()
-}, {
-  returnDetailedScanResult: true,
-  highlightScanRegion: true
-});
-
-
-const toggleButtonAndVideoDisplay = () => {
-  startButtonElem.style.display = window.getComputedStyle(startButtonElem).display === 'block' ? 'none' : 'block'
-  if (window.getComputedStyle(videoElem).opacity == 1) {
-    videoElem.style.opacity = 0
-    videoElem.style.width = '0'
-    videoElem.style.height = '0'
-    qrScanner.stop()
-  } else {
-    videoElem.style.opacity = 1
-    videoElem.style.width = 'auto'
-    videoElem.style.height = 'auto'
-  }
+  toggleButtonAndVideoDisplay(media)
 }
 
+const toggleButtonAndVideoDisplay = (media) => {
+  if (media === 'cam') {
+    // buttonsContainerElem.style.display = window.getComputedStyle(buttonsContainerElem).display === 'block' ? 'none' : 'block'
+    if (window.getComputedStyle(videoElem).opacity == 1) {
+      videoElem.style.opacity = 0
+      videoElem.style.width = '0'
+      videoElem.style.height = '0'
+      qrScanner.stop()
+    } else {
+      imgElem.style.display = 'none'
+      videoElem.style.opacity = 1
+      videoElem.style.width = 'auto'
+      videoElem.style.height = 'auto'
+    }
+  }
+
+  else if (media === 'file') {
+    if (window.getComputedStyle(imgElem).display === 'none') {
+      imgElem.style.display = 'block'
+      imgElem.style.width = 'auto'
+      imgElem.style.height = 'auto'
+      const reader = new FileReader();
+      reader.addEventListener('load', (e) => {
+        imgElem.setAttribute('src', e.target.result);
+      })
+      reader.readAsDataURL(file.files[0]);
+    }
+  }
+}
 // code to register sw.js
 
 if ('serviceWorker' in navigator) {
